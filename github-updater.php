51793223c5a2ea29c3e3a326a8f902c93b32f911
Andy Fragen <andy@thefragens.com>
2013-08-08 09:43:40 -0700

    small description change

--- github-updater.php
+++ github-updater.php
@@ -4,7 +4,7 @@
 Plugin Name: GitHub Updater
 Plugin URI: https://github.com/afragen/github-updater
 GitHub Plugin URI: https://github.com/afragen/github-updater
-Description: Plugin and Theme Updater classes to pull updates of the GitHub based plugins and themes into wordpress. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>.
+Description: Plugin and Theme Updater classes to pull updates of the GitHub hosted plugins and themes into wordpress. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>.
 Version: 1.4.2
 Author: Andy Fragen
 License: GNU General Public License v2
