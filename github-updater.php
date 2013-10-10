af9d154b3c0a01b8a59cedbe64f5b5e75d433e06
Andy Fragen <andy@thefragens.com>
2013-10-10 11:31:07 -0700

    version bump

--- github-updater.php
+++ github-updater.php
@@ -14,7 +14,7 @@ Plugin URI:        https://github.com/afragen/github-updater
 GitHub Plugin URI: afragen/github-updater
 GitHub Branch:     master
 Description:       Plugin and Theme Updater classes to pull updates of the GitHub hosted plugins and themes into wordpress. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>.
-Version:           1.6.1
+Version:           1.7.0
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
