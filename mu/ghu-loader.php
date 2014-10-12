83e07a5d1a5516d1292f565c8cbaa25c683e77e8
Andy Fragen <afragen@Fawkes.local>
2014-10-12 14:49:09 -0700

    add mu loader
    still need to add instructions to README

---
+++ mu/ghu-loader.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * GitHub Updater
+ *
+ * @package   GitHub_Updater
+ * @author    Andy Fragen
+ * @license   GPL-2.0+
+ * @link      https://github.com/afragen/github-updater
+ */
+
+/*
+Plugin Name:       GitHub Updater MU loader
+Plugin URI:        https://github.com/afragen/github-updater
+Description:       A plugin to automatically update GitHub or Bitbucket hosted plugins and themes into WordPress. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications.
+Version:           1.0.0
+Author:            Andy Fragen
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Load normal plugin
+if ( ! class_exists( 'GitHub_Updater' ) ) {
+	require_once WP_PLUGIN_DIR . '/github-updater/github-updater.php';
+}
+
+function ghu_deactivate() {
+	deactivate_plugins( 'github-updater/github-updater.php' );
+}
+
+//deactivate normal plugin as it's loaded as mu-plugin
+add_action( 'admin_init', 'ghu_deactivate' );
