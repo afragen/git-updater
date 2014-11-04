0b18272cfe2e464d44539703ad93a2d36092a348
Andy Fragen <andy@thefragens.com>
2014-11-03 18:54:54 -0800

    oops forgot to actually do something with it

--- includes/class-github-updater.php
+++ includes/class-github-updater.php
@@ -515,7 +515,7 @@ class GitHub_Updater {
 		$changelogs = array( 'CHANGES.md', 'CHANGELOG.md' );
 
 		foreach ( $changelogs as $changes ) {
-			strtolower( $changes );
+			$changes = strtolower( $changes );
 			if ( file_exists( $this->$type->local_path . $changes ) ) {
 				return $changes;
 			}
