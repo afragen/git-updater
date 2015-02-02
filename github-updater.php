a2570fb38503880f1cbd868074fb8ec89686833b
Andy Fragen <andy@thefragens.com>
2015-02-02 12:06:41 -0800

    update autoloader to use PSR 4 autoloading
    and real namespacing

--- github-updater.php
+++ github-updater.php
@@ -12,7 +12,7 @@
 Plugin Name:       GitHub Updater
 Plugin URI:        https://github.com/afragen/github-updater
 Description:       A plugin to automatically update GitHub or Bitbucket hosted plugins and themes into WordPress. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications.
-Version:           3.2.6.1
+Version:           3.2.6.2
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -29,11 +29,19 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Load Autoloader
-require_once( 'classes/Autoloader.php' );
-
 // Load textdomain
 load_plugin_textdomain( 'github-updater', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+
+// Plugin namespace root
+$root = array( 'Fragen\GitHub_Updater' => __DIR__ . '/classes/GitHub_Updater' );
+
+// Add compat classes
+$compatibility = array( 'Fragen\GitHub_Updater\Parsedown' => __DIR__ . '/classes/Parsedown.php' );
+
+// Load Autoloader
+require_once( __DIR__ . '/classes/GitHub_Updater/Autoloader.php' );
+$class_loader = 'Fragen\GitHub_Updater\Autoloader';
+new $class_loader( $root, $compatibility );
 
 // Instantiate class GitHub_Updater
 new GitHub_Updater__Base;
