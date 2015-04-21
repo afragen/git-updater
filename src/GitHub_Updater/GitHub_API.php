c1b5304b20d36d066fdabe7d46a47176a545c30a
Andy Fragen <andy@thefragens.com>
2015-04-20 21:07:55 -0700

    remove access_token when enterprise

--- src/GitHub_Updater/GitHub_API.php
+++ src/GitHub_Updater/GitHub_API.php
@@ -98,7 +98,7 @@ class GitHub_API extends Base {
 		 * If using GitHub Enterprise header return this endpoint.
 		 */
 		if ( ! empty( $this->type->enterprise ) ) {
-			return $this->type->enterprise . $endpoint;
+			return $this->type->enterprise . remove_query_arg( 'access_token', $endpoint );
 		}
 
 		return 'https://api.github.com' . $endpoint;
@@ -251,7 +251,7 @@ class GitHub_API extends Base {
 		if ( ! empty( parent::$options[ $this->type->repo ] ) ) {
 			$endpoint = add_query_arg( 'access_token', parent::$options[ $this->type->repo ], $endpoint );
 			return $download_link_base . $endpoint;
-		} elseif ( ! empty( parent::$options['github_access_token'] ) ) {
+		} elseif ( ! empty( parent::$options['github_access_token'] ) && empty( $this->type->enterprise ) ) {
 			$endpoint = add_query_arg( 'access_token', parent::$options['github_access_token'], $endpoint );
 			return $download_link_base . $endpoint;
 		}
--- src/GitHub_Updater/Install.php
+++ src/GitHub_Updater/Install.php
@@ -96,7 +96,9 @@ class Install extends Base {
 				if ( ! empty( self::$install['github_access_token'] ) ) {
 					self::$install['download_link'] = add_query_arg( 'access_token', self::$install['github_access_token'], self::$install['download_link'] );
 					parent::$options[ self::$install['repo'][1] ] = self::$install['github_access_token'];
-				} elseif ( ! empty( parent::$options['github_access_token'] ) ) {
+				} elseif ( ! empty( parent::$options['github_access_token'] ) &&
+				           ( 'github.com' === $install_host || empty( $install_host ) )
+				) {
 					self::$install['download_link'] = add_query_arg( 'access_token', parent::$options['github_access_token'], self::$install['download_link'] );
 				}
 			}
