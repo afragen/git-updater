d9703548553d828d7e149a7f45a1d67280600561
Andy Fragen <andy@thefragens.com>
2015-10-02 08:14:12 -0700

    fix for installing from GitHub Enterprise

--- src/GitHub_Updater/Install.php
+++ src/GitHub_Updater/Install.php
@@ -90,7 +90,7 @@ class Install extends Base {
 					$github_base     = 'https://api.github.com';
 					$headers['host'] = 'github.com';
 				} else {
-					$github_base = $headers['base_uri'] . '/api/v3';
+					$github_base = $headers['base_uri'];
 				}
 
 				self::$install['download_link'] = $github_base . '/repos/' . self::$install['github_updater_repo'] . '/zipball/' . self::$install['github_updater_branch'];
