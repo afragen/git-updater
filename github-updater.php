d289f113ceff6ad4e00db1dfffd28b381339e645
Andy Fragen <andy@thefragens.com>
2015-12-03 08:14:07 -0800

    back to generic local parameters

--- github-updater.php
+++ github-updater.php
@@ -12,7 +12,7 @@
 Plugin Name:       GitHub Updater
 Plugin URI:        https://github.com/afragen/github-updater
 Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
-Version:           5.3.0.1
+Version:           5.3.0.2
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -48,20 +48,20 @@ if ( ! $updatePhp->does_it_meet_required_php_version( PHP_VERSION ) ) {
 load_plugin_textdomain( 'github-updater', false, __DIR__ . '/languages' );
 
 // Plugin namespace root
-$ghu_root = array( 'Fragen\\GitHub_Updater' => __DIR__ . '/src/GitHub_Updater' );
+$root = array( 'Fragen\\GitHub_Updater' => __DIR__ . '/src/GitHub_Updater' );
 
 // Add extra classes
-$ghu_extra_classes = array(
-	'Parsedown'         => __DIR__ . '/vendor/parsedown/Parsedown.php',
-	'WPUpdatePHP'       => __DIR__ . '/vendor/wp-update-php/src/WPUpdatePhp.php',
-	'Automattic_Readme' => __DIR__ . '/vendor/parse-readme.php',
-	);
+$extra_classes = array(
+		'Parsedown'         => __DIR__ . '/vendor/parsedown/Parsedown.php',
+		'WPUpdatePHP'       => __DIR__ . '/vendor/wp-update-php/src/WPUpdatePhp.php',
+		'Automattic_Readme' => __DIR__ . '/vendor/parse-readme.php',
+		);
 
 // Load Autoloader
 require_once( __DIR__ . '/src/GitHub_Updater/Autoloader.php' );
-$ghu_loader = 'Fragen\\GitHub_Updater\\Autoloader';
-new $ghu_loader( $ghu_root, $ghu_extra_classes );
+$loader = 'Fragen\\GitHub_Updater\\Autoloader';
+new $loader( $root, $extra_classes );
 
 // Instantiate class GitHub_Updater
-$ghu_instantiate = 'Fragen\\GitHub_Updater\\Base';
-new $ghu_instantiate;
+$instantiate = 'Fragen\\GitHub_Updater\\Base';
+new $instantiate;
