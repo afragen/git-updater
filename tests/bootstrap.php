6a4fb3c7941e3fa244a432a4e71560b37f6dde28
Andy Fragen <andy@thefragens.com>
2015-12-27 13:25:14 -0800

    update bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,18 @@
 <?php
 /**
- * Bootstrap the plugin unit testing environment.
- *
- * Edit 'active_plugins' setting below to point to your main plugin file.
- *
+ * Bootstrap the testing environment
+ * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
  * @package wordpress-plugin-tests
+ *
+ * Usage: change the below array to any plugin(s) you want activated during the tests
+ *        value should be the path to the plugin relative to /wp-content/
+ *
+ * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
+ *
  */
 
-// Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'plugin-slug/main-plugin-file.php' ),
+	'active_plugins' => array( 'github-updater/github-updater.php' ),
 );
 
-// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that this plugin is installed in a
-// WordPress develop SVN checkout.
-
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
-}
+require dirname( __FILE__ ) . '/lib/bootstrap.php';
