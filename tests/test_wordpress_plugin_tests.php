c39acefece05c8385c73f8dde6d4fae46c43b031
Andy Fragen <andy@thefragens.com>
2015-12-27 13:24:18 -0800

    more travis ci

---
+++ tests/test_wordpress_plugin_tests.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Tests to test that that testing framework is testing tests. Meta, huh?
+ *
+ * @package wordpress-plugins-tests
+ */
+class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
+
+	/**
+	 * Run a simple test to ensure that the tests are running
+	 */
+	 function test_tests() {
+
+		$this->assertTrue( true );
+
+	 }
+
+	/**
+	 * If these tests are being run on Travis CI, verify that the version of
+	 * WordPress installed is the version that we requested.
+	 *
+	 * @requires PHP 5.3
+	 */
+	function test_wp_version() {
+
+		if ( !getenv( 'TRAVIS' ) )
+			$this->markTestSkipped( 'Test skipped since Travis CI was not detected.' );
+
+		$requested_version = getenv( 'WP_VERSION' ) . '-src';
+
+		// The "master" version requires special handling.
+		if ( $requested_version == 'master-src' ) {
+			$file = file_get_contents( 'https://develop.svn.wordpress.org/trunk/src/wp-includes/version.php' );
+			preg_match( '#\$wp_version = \'([^\']+)\';#', $file, $matches );
+			$requested_version = $matches[1];
+		}
+
+		$this->assertEquals( get_bloginfo( 'version' ), $requested_version );
+
+	}
+
+	/**
+	 * Ensure that the plugin has been installed and activated.
+	 */
+	function test_plugin_activated() {
+
+		$this->assertTrue( is_plugin_active( 'plugin-slug/main-plugin-file.php' ) );
+
+	}
+
+}
