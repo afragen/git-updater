3d1b571bf80cf72aa8dfe791e0d6a8c46a085d39
Andy Fragen <andy@thefragens.com>
2015-12-27 13:32:37 -0800

    no symlinks

--- tests/lib/bootstrap.php
+++ tests/lib/bootstrap.php
@@ -1,1 +1,18 @@
-/Users/afragen/Documents/github/github-updater/tests/bootstrap.php
+<?php
+/**
+ * Bootstrap the testing environment
+ * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
+ * @package wordpress-plugin-tests
+ *
+ * Usage: change the below array to any plugin(s) you want activated during the tests
+ *        value should be the path to the plugin relative to /wp-content/
+ *
+ * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
+ *
+ */
+
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'github-updater/github-updater.php' ),
+);
+
+require dirname( __FILE__ ) . '/lib/bootstrap.php';
