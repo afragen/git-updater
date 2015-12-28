cfbaf70ba9f6fa045301796da5ea889eac6da889
Andy Fragen <andy@thefragens.com>
2015-12-28 08:25:41 -0800

    add test class for Base

---
+++ tests/test-base.php
@@ -0,0 +1,13 @@
+<?php
+
+use Fragen\GitHub_Updater;
+
+
+class Test_Base extends \WP_TestUnitCase{
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+
+}
