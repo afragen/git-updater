1248c23df51bfed598c301d722e75384038c2caa
Andy Fragen <andy@thefragens.com>
2016-09-14 20:15:04 -0700

    add docblocks, spacing

--- src/GitHub_Updater/Language_Pack.php
+++ src/GitHub_Updater/Language_Pack.php
@@ -1,33 +1,59 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: afragen
- * Date: 9/12/16
- * Time: 4:12 PM
+ * GitHub Updater
+ *
+ * @package   GitHub_Updater
+ * @author    Andy Fragen
+ * @license   GPL-2.0+
+ * @link      https://github.com/afragen/github-updater
  */
 
 namespace Fragen\GitHub_Updater;
 
 
+/**
+ * Class Language_Pack
+ *
+ * @package Fragen\GitHub_Updater
+ */
 class Language_Pack extends Base {
 
+	/**
+	 * Variable containing the plugin/theme object.
+	 *
+	 * @var object
+	 */
 	protected $repo;
 
+	/**
+	 * Variable containing the git host API object.
+	 *
+	 * @var
+	 */
 	protected $repo_api;
 
+	/**
+	 * Language_Pack constructor.
+	 *
+	 * @param object $repo Plugin/Theme object.
+	 * @param object $api  Git host API object.
+	 */
 	public function __construct( $repo, $api ) {
-		if ( empty( $repo->languages) ) {
+		if ( empty( $repo->languages ) ) {
 			return false;
 		}
 
-		$this->repo = $repo;
+		$this->repo     = $repo;
 		$this->repo_api = $api;
 		$this->run();
 	}
 
+	/**
+	 * Do the Language Pack integration.
+	 */
 	protected function run() {
 		$headers = $this->parse_header_uri( $this->repo->languages );
-		$this->repo_api->get_language_pack( $headers);
+		$this->repo_api->get_language_pack( $headers );
 	}
 
 }
