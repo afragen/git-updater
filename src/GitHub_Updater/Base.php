c2806cc9040d6926fb49c06896d52be3f68d6da9
Andy Fragen <andy@thefragens.com>
2016-10-27 08:59:58 -0700

    unset condition to run method
    only run once

--- src/GitHub_Updater/Base.php
+++ src/GitHub_Updater/Base.php
@@ -884,6 +884,8 @@ class Base {
 	public function delete_all_transients() {
 		global $wpdb;
 
+		unset( $_GET['refresh_transients'] );
+
 		$table         = is_multisite() ? $wpdb->base_prefix . 'sitemeta' : $wpdb->base_prefix . 'options';
 		$column        = is_multisite() ? 'meta_key' : 'option_name';
 		$delete_string = 'DELETE FROM ' . $table . ' WHERE ' . $column . ' LIKE %s LIMIT 1000';
