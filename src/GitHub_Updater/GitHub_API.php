cb6b100bd234db49370707b9d21d9b2e7b4ac056
Andy Fragen <andy@thefragens.com>
2016-12-27 08:47:03 -0800

    change to public function

--- src/GitHub_Updater/GitHub_API.php
+++ src/GitHub_Updater/GitHub_API.php
@@ -466,7 +466,7 @@ class GitHub_API extends API {
 	 * @param $response
 	 * @param $repo
 	 */
-	protected static function ratelimit_reset( $response, $repo ) {
+	public static function ratelimit_reset( $response, $repo ) {
 		if ( isset( $response['headers']['x-ratelimit-reset'] ) ) {
 			$reset                       = (integer) $response['headers']['x-ratelimit-reset'];
 			$wait                        = date( 'i', $reset - time() );
