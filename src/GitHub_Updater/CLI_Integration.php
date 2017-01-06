9e84b118244484f5744dcb00d76f44dca1bcb0cd
Andy Fragen <andy@thefragens.com>
2017-01-06 14:36:19 -0800

    update for 4.7 doing it wrong

--- src/GitHub_Updater/CLI_Integration.php
+++ src/GitHub_Updater/CLI_Integration.php
@@ -217,7 +217,7 @@ class CLI_Integration extends WP_CLI_Command {
 /**
  * Use custom installer skins to display error messages.
  */
-require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader-skins.php';
+require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
 
 /**
  * Class GitHub_Upgrader_CLI_Plugin_Installer_Skin
