2185acb93e6d6d0d2ffa6dee4633f526ac3333c6
Andy Fragen <andy@thefragens.com>
2017-02-22 00:03:00 -0800

    update parse_extra_headers for Bitbucket enterprise

--- src/GitHub_Updater/Base.php
+++ src/GitHub_Updater/Base.php
@@ -1440,6 +1440,11 @@ class Base {
 				case 'GitLab':
 					$header['enterprise_api'] = $header['enterprise_uri'] . '/api/v3';
 					break;
+				case 'Bitbucket':
+					$header['enterprise_api'] = $header['enterprise_uri'];
+					break;
+				default:
+					break;
 			}
 		}
 
