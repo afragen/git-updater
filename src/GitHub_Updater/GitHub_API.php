9d53b08427f7f71d415979b14684afc8e3e2acdd
Andy Fragen <andy@thefragens.com>
2017-10-26 17:30:04 -0700

    Add query arg only if one exists

--- src/GitHub_Updater/GitHub_API.php
+++ src/GitHub_Updater/GitHub_API.php
@@ -685,7 +685,9 @@ class GitHub_API extends API implements API_Interface {
 				: static::$options['github_enterprise_token'];
 		}
 
-		$install['download_link'] = add_query_arg( 'access_token', $token, $install['download_link'] );
+		if ( ! empty( $token ) ) {
+			$install['download_link'] = add_query_arg( 'access_token', $token, $install['download_link'] );
+		}
 
 		if ( ! empty( static::$options['github_access_token'] ) ) {
 			unset( $install['options']['github_access_token'] );
