76f47fd8a50747c877f85b1cfea62ffa149327fb
Andy Fragen <andy@thefragens.com>
2018-01-20 12:20:27 -0800

    refactor of autoloader

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -29,7 +29,7 @@ if ( ! class_exists( 'Fragen\\Autoloader' ) ) {
 	 * @author    Barry Hughes <barry@codingkillsme.com>
 	 * @link      http://github.com/afragen/autoloader
 	 * @copyright 2015 Andy Fragen
-	 * @version   2.1.0
+	 * @version   2.2.0
 	 */
 	class Autoloader {
 		/**
@@ -47,19 +47,31 @@ if ( ! class_exists( 'Fragen\\Autoloader' ) ) {
 		 */
 		protected $map = array();
 
+		/**
+		 * Array of class file names that don't correspond to class names.
+		 *
+		 * @var array
+		 */
+		protected $misnamed = array();
 
 		/**
 		 * Constructor.
 		 *
 		 * @access public
 		 *
-		 * @param array      $roots      Roots to scan when autoloading.
-		 * @param array|null $static_map List of classes that deviate from convention. Defaults to null.
+		 * @param array      $roots            Roots to scan when autoloading.
+		 * @param array|null $static_map       Array of classes that deviate from convention.
+		 *                                     Defaults to null.
+		 * @param array|null $misnamed_classes Array of classes whose file names deviate from convention.
+		 *                                     Defaults to null.
 		 */
-		public function __construct( array $roots, array $static_map = null ) {
+		public function __construct( array $roots, array $static_map = null, array $misnamed_classes = null ) {
 			$this->roots = $roots;
 			if ( null !== $static_map ) {
 				$this->map = $static_map;
+			}
+			if ( null !== $misnamed_classes ) {
+				$this->misnamed = $misnamed_classes;
 			}
 			spl_autoload_register( array( $this, 'autoload' ) );
 		}
@@ -88,16 +100,22 @@ if ( ! class_exists( 'Fragen\\Autoloader' ) ) {
 					continue;
 				}
 
+				$psr4_fname = substr( $class, strlen( $namespace ) + 1 );
+				$psr4_fname = str_replace( '\\', DIRECTORY_SEPARATOR, $psr4_fname );
+
 				// Determine the possible path to the class, include all subdirectories.
-				$dirs = glob( $root_dir . '/*', GLOB_ONLYDIR );
-				array_unshift( $dirs, $root_dir );
+				$objects = new \RecursiveIteratorIterator( new \RecursiveDirectoryIterator( $root_dir ), \RecursiveIteratorIterator::SELF_FIRST );
+				foreach ( $objects as $name => $object ) {
+					if ( is_dir( $name ) ) {
+						$directories[] = rtrim( $name, './' );
+					}
+				}
+				$directories = array_unique( $directories );
 
-				$path = substr( $class, strlen( $namespace ) + 1 );
-				$path = str_replace( '\\', DIRECTORY_SEPARATOR, $path );
+				$fnames = array();
+				$fnames = array_merge( array( $psr4_fname ), $fnames, $this->misnamed );
 
-				$paths = array_map( function( $dir ) use ( $path ) {
-					return $dir . DIRECTORY_SEPARATOR . $path . '.php';
-				}, $dirs );
+				$paths = $this->get_paths( $directories, $fnames );
 
 				// Test for its existence and load if present.
 				foreach ( $paths as $path ) {
@@ -107,6 +125,24 @@ if ( ! class_exists( 'Fragen\\Autoloader' ) ) {
 					}
 				}
 			}
+		}
+
+		/**
+		 * Get and return an array of possible file paths.
+		 *
+		 * @param array $dirs       Array of plugin directories and subdirectories.
+		 * @param array $file_names Array of possible file names.
+		 *
+		 * @return mixed
+		 */
+		private function get_paths( $dirs, $file_names ) {
+			foreach ( $file_names as $file_name ) {
+				$paths[] = array_map( function( $dir ) use ( $file_name ) {
+					return $dir . DIRECTORY_SEPARATOR . $file_name . '.php';
+				}, $dirs );
+			}
+
+			return call_user_func_array( 'array_merge', $paths );
 		}
 	}
 }
