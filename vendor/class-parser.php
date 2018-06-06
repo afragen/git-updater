81d1b359dcc558fdf43760e7e16d79d0b74803f7
Andy Fragen <andy@thefragens.com>
2018-06-06 12:24:57 -0700

    updated class-parser

--- vendor/class-parser.php
+++ vendor/class-parser.php
@@ -88,6 +88,13 @@ class Parser {
 	public $faq = array();
 
 	/**
+	 * Warning flags which indicate specific parsing failures have occured.
+	 *
+	 * @var array
+	 */
+	public $warnings = array();
+
+	/**
 	 * These are the readme sections that we expect.
 	 *
 	 * @var array
@@ -239,7 +246,7 @@ class Parser {
 			$this->tested = $headers['tested'];
 		}
 		if ( ! empty( $headers['requires_php'] ) ) {
-			$this->requires_php = $headers['requires_php'];
+			$this->requires_php = $this->sanitize_requires_php( $headers['requires_php'] );
 		}
 		if ( ! empty( $headers['contributors'] ) ) {
 			$this->contributors = explode( ',', $headers['contributors'] );
@@ -547,6 +554,7 @@ class Parser {
 			// In the event that something invalid is used, we'll ignore it (Example: 'Joe Bloggs (Australian Translation)')
 			if ( ! $user ) {
 				unset( $users[ $i ] );
+				$this->warnings['contributor_ignored'] = true;
 				continue;
 			}
 
@@ -575,6 +583,25 @@ class Parser {
 		}
 
 		return $stable_tag;
+	}
+
+	/**
+	 * Sanitizes the Requires PHP header to ensure that it's a valid version header.
+	 *
+	 * @param string $version
+	 * @return string The sanitized $version
+	 */
+	protected function sanitize_requires_php( $version ) {
+		$version = trim( $version );
+
+		// x.y or x.y.z
+		if ( $version && ! preg_match( '!^\d+(\.\d+){1,2}$!', $version ) ) {
+			$this->warnings['requires_php_ignored'] = true;
+			// Ignore the readme value.
+			$version = '';
+		}
+
+		return $version;
 	}
 
 	/**
