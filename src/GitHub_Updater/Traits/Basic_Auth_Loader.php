3d401314b6577d3cdc7f59ce3b1df0c5a8b42a74
Andy Fragen <andy@thefragens.com>
2018-06-06 18:25:16 -0700

    move to top

--- src/GitHub_Updater/Traits/Basic_Auth_Loader.php
+++ src/GitHub_Updater/Traits/Basic_Auth_Loader.php
@@ -34,6 +34,16 @@ trait Basic_Auth_Loader {
 	private static $basic_auth_required = [ 'Bitbucket' ];
 
 	/**
+	 * Load hooks for Bitbucket authentication headers.
+	 *
+	 * @access public
+	 */
+	public function load_authentication_hooks() {
+		add_filter('http_request_args', [ $this, 'maybe_basic_authenticate_http' ], 5, 2);
+		add_filter('http_request_args', [ $this, 'http_release_asset_auth' ], 15, 2);
+	}
+
+	/**
 	 * Remove hooks for Bitbucket authentication headers.
 	 *
 	 * @access public
@@ -263,15 +273,5 @@ trait Basic_Auth_Loader {
 		remove_filter('upgrader_pre_download', [ $this, 'upgrader_pre_download' ]);
 
 		return $reply;
-	}
-
-	/**
-	 * Load hooks for Bitbucket authentication headers.
-	 *
-	 * @access public
-	 */
-	public function load_authentication_hooks() {
-		add_filter('http_request_args', [ $this, 'maybe_basic_authenticate_http' ], 5, 2);
-		add_filter('http_request_args', [ $this, 'http_release_asset_auth' ], 15, 2);
 	}
 }
