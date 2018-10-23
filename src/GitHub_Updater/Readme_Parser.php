270b52c5efcdfb1765c68a8d57f1a33eba4d86e4
Andy Fragen <andy@thefragens.com>
2018-10-23 11:45:44 -0700

    no longer in 5.0, bumped to 5.1

--- src/GitHub_Updater/Readme_Parser.php
+++ src/GitHub_Updater/Readme_Parser.php
@@ -98,7 +98,7 @@ class Readme_Parser extends Parser {
 			$contributors[ $contributor ]['display_name'] = $contributor;
 			$contributors[ $contributor ]['profile']      = '//profiles.wordpress.org/' . $contributor;
 			$contributors[ $contributor ]['avatar']       = 'https://wordpress.org/grav-redirect.php?user=' . $contributor;
-			if ( $wp_version < '5.0-alpha-42631' ) {
+			if ( $wp_version < '5.1-alpha' ) {
 				$contributors[ $contributor ] = '//profiles.wordpress.org/' . $contributor;
 			}
 		}
