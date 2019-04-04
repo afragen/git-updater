d0e172756367ed16e1aa43a441bd86fd7a74e969
Andy Fragen <andy@thefragens.com>
2019-04-03 21:11:27 -0700

    a11y settings tabs

--- src/GitHub_Updater/Settings.php
+++ src/GitHub_Updater/Settings.php
@@ -186,12 +186,12 @@ class Settings extends Base {
 	 */
 	private function options_tabs() {
 		$current_tab = isset( $_GET['tab'] ) ? esc_attr( $_GET['tab'] ) : 'github_updater_settings';
-		echo '<h2 class="nav-tab-wrapper">';
+		echo '<nav class="nav-tab-wrapper" aria-label="Secondary menu">';
 		foreach ( $this->settings_tabs() as $key => $name ) {
 			$active = ( $current_tab === $key ) ? 'nav-tab-active' : '';
 			echo '<a class="nav-tab ' . $active . '" href="?page=github-updater&tab=' . $key . '">' . $name . '</a>';
 		}
-		echo '</h2>';
+		echo '</nav>';
 	}
 
 	/**
@@ -201,12 +201,12 @@ class Settings extends Base {
 	 */
 	private function options_sub_tabs() {
 		$current_tab = isset( $_GET['subtab'] ) ? esc_attr( $_GET['subtab'] ) : 'github_updater';
-		echo '<h3 class="nav-tab-wrapper">';
+		echo '<nav class="nav-tab-wrapper" aria-label="Tertiary menu">';
 		foreach ( $this->settings_sub_tabs() as $key => $name ) {
 			$active = ( $current_tab === $key ) ? 'nav-tab-active' : '';
 			echo '<a class="nav-tab ' . $active . '" href="?page=github-updater&tab=github_updater_settings&subtab=' . $key . '">' . $name . '</a>';
 		}
-		echo '</h3>';
+		echo '</nav>';
 	}
 
 	/**
