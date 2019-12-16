aad7cea52959b56959111a345e29567c0450b37a
Andy Fragen <andy@thefragens.com>
2019-12-16 08:04:09 -0800

    Revert "don't punish frontend calls"
    This reverts commit 4efccf8afd7013b771165e1fc7242f8c9e5b3014.

--- src/GitHub_Updater/Base.php
+++ src/GitHub_Updater/Base.php
@@ -270,8 +270,8 @@ class Base {
 	 * @return bool
 	 */
 	public function get_remote_repo_meta( $repo ) {
-		// Exit if frontend and bypassing wp-cron.
-		if ( apply_filters( 'github_updater_disable_wpcron', false ) && ! is_admin() ) {
+		// Exit if non-privileged user and bypassing wp-cron.
+		if ( apply_filters( 'github_updater_disable_wpcron', false ) && ! Singleton::get_instance( 'Init', $this )->can_update() ) {
 			return;
 		}
 
