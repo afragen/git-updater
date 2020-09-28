b5ffe94560cf3004dccad508e0b194691e66596b
Andy Fragen <andy@thefragens.com>
2020-09-27 17:01:58 -0700

    fix for multisite branch switch row refactor

--- src/GitHub_Updater/Base.php
+++ src/GitHub_Updater/Base.php
@@ -558,7 +558,9 @@ class Base {
 			}
 		}
 
-		$open = '<tr class="plugin-update-tr' . $active . '">
+		$multisite_theme_open = ! $branch_switcher && 'theme' === $type ? "\" id='{$repo_name}' data-slug='{$repo_name}'" : '"';
+
+		$open = '<tr class="plugin-update-tr' . $active . $multisite_theme_open . '>
 		<td colspan="' . $wp_list_table->get_column_count() . '" class="plugin-update colspanchange">
 		<div class="">';
 
