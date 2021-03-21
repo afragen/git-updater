36ddbad8e8d0e408e75e027f2014a898c6283ec1
Andy Fragen <andy@thefragens.com>
2021-03-21 12:18:25 -0700

    fix i18n

--- src/Git_Updater/Bootstrap.php
+++ src/Git_Updater/Bootstrap.php
@@ -65,7 +65,7 @@ class Bootstrap {
 
 		$message = sprintf(
 			/* translators: %s: documentation URL */
-			__( 'Git Updater is missing required composer dependencies. <a href="%s" target="_blank" rel="noopenernoreferer">Learn more.</a>', 'github-updater' ),
+			__( 'Git Updater is missing required composer dependencies. <a href="%s" target="_blank" rel="noopenernoreferer">Learn more.</a>', 'git-updater' ),
 			'https://github.com/afragen/github-updater/wiki/Installation'
 		);
 
