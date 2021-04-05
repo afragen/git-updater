382398f4bbce55bc20f2dc5ee4333a16e80abd3a
Andy Fragen <andy@thefragens.com>
2021-04-05 14:43:22 -0700

    update namespacing

--- uninstall.php
+++ uninstall.php
@@ -18,8 +18,8 @@
  * For more information, see the following discussion:
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
- * @link    https://github.com/afragen/github-updater
- * @package github-updater
+ * @link    https://github.com/afragen/git-updater
+ * @package git-updater
  */
 
 // If uninstall not called from WordPress, then exit.
