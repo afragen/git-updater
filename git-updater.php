67a2aa98366512df91d6280a8dd33ac85e4229f6
Andy Fragen <andy@thefragens.com>
2021-04-06 07:44:14 -0700

    add stub for updating

---
+++ git-updater.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * GitHub Updater
+ *
+ * @author  Andy Fragen
+ * @license GPL-2.0+
+ * @link    https://github.com/afragen/github-updater
+ * @package github-updater
+ */
+
+/**
+ * Version:           9.9.10
+ */
