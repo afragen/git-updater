6d5664408909ab589ba8fbac383b6c792b9484f9
sharevb <sharevb@gmail.com>
2021-04-11 21:56:35 +0200

    Polyfill for Element.closest
    Polyfill for Element.matches and Element.closest in IE

--- js/ghu-install-vanilla.js
+++ js/ghu-install-vanilla.js
@@ -19,6 +19,21 @@
 		}
 	    };
 	}
+	
+	//polyfill for Element.matches and Element.closest in IE
+	if (!Element.prototype.matches)
+	    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+					Element.prototype.webkitMatchesSelector;
+	if (!Element.prototype.closest)
+	    Element.prototype.closest = function(s) {
+		var el = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+		    if (el.matches(s)) return el;
+		    el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType == 1);
+		return null;
+	    };
 
 	// Hide non-default (Bitbucket & GitLab) settings on page load.
 	let nonDefault = ['bitbucket', 'gitlab', 'gitea', 'zipfile', 'gist'];
