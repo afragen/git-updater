bad2156676e17b63a7bd0a72493c21add21fbcc7
Andy Fragen <andy@thefragens.com>
2021-04-27 10:49:51 -0700

    add free plan

--- src/Git_Updater/Bootstrap.php
+++ src/Git_Updater/Bootstrap.php
@@ -201,21 +201,23 @@ class Bootstrap {
 
 					$gu_fs = fs_dynamic_init(
 						[
-							'id'               => '8195',
-							'slug'             => 'git-updater',
-							'premium_slug'     => 'git-updater',
-							'type'             => 'plugin',
-							'public_key'       => 'pk_2cf29ecaf78f5e10f5543c71f7f8b',
-							'is_premium'       => true,
-							'is_premium_only'  => true,
-							'has_addons'       => true,
-							'has_paid_plans'   => true,
-							'is_org_compliant' => false,
-							'trial'            => [
+							'id'                  => '8195',
+							'slug'                => 'git-updater',
+							'premium_slug'        => 'git-updater',
+							'type'                => 'plugin',
+							'public_key'          => 'pk_2cf29ecaf78f5e10f5543c71f7f8b',
+							'is_premium'          => true,
+							'premium_suffix'      => 'PRO',
+							// If your plugin is a serviceware, set this option to false.
+							'has_premium_version' => true,
+							'has_addons'          => true,
+							'has_paid_plans'      => true,
+							'is_org_compliant'    => false,
+							'trial'               => [
 								'days'               => 14,
 								'is_require_payment' => false,
 							],
-							'menu'             => [
+							'menu'                => [
 								'slug'    => 'git-updater',
 								'contact' => false,
 								'support' => false,
