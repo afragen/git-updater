3327e20c8dbc0792d95ec5b7124a3b1b80906de8
Andy Fragen <andy@thefragens.com>
2021-04-27 09:50:56 -0700

    remove /src/Git_Updater_PRO in free version

--- git-updater.php
+++ git-updater.php
@@ -22,6 +22,8 @@
  * GitHub Languages:  https://github.com/afragen/git-updater-translations
  * Requires at least: 5.2
  * Requires PHP:      7.0
+ *
+ * @fs_premium_only /src/Git_Updater_PRO
  */
 
 namespace Fragen\Git_Updater;
