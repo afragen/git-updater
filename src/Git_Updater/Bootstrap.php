d2ddce8572a1448647debadb6aa8daaabfcb9ea2
Andy Fragen <andy@thefragens.com>
2022-03-10 13:32:02 -0800

    might as well spell it correctly

--- src/Git_Updater/Bootstrap.php
+++ src/Git_Updater/Bootstrap.php
@@ -70,7 +70,7 @@ class Bootstrap {
 		$message = sprintf(
 			/* translators: %1: opening tag, %2: closing tag */
 			__( 'Git Updater is missing required composer dependencies. %1$sLearn more.%2$s', 'git-updater' ),
-			'<a href="https://github.com/afragen/git-updater/wiki/Installation" target="_blank" rel="noreferer">',
+			'<a href="https://github.com/afragen/git-updater/wiki/Installation" target="_blank" rel="noreferrer">',
 			'</a>'
 		);
 
