b34fa5b2a78f9308870acb21325b8aa2a9773f00
Andy Fragen <andy@thefragens.com>
2022-07-12 12:17:05 -0700

    short array

--- src/Git_Updater/Shim.php
+++ src/Git_Updater/Shim.php
@@ -58,7 +58,7 @@ function move_dir( $from, $to ) {
 			}
 		}
 
-		$result = copy_dir( $from, $to, array( basename( $to ) ) );
+		$result = copy_dir( $from, $to, [ basename( $to ) ] );
 
 		// Clear the source directory.
 		if ( ! is_wp_error( $result ) ) {
