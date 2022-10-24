ab83a4e341a493e9bd0942a33b77395353b1e048
Andy Fragen <andy@thefragens.com>
2022-10-24 13:00:29 -0700

    add descriptions to API add-ons

--- src/Git_Updater/Add_Ons.php
+++ src/Git_Updater/Add_Ons.php
@@ -59,6 +59,7 @@ class Add_Ons {
 			'gist'      => [
 				[
 					'name'     => __( 'Git Updater - Gist', 'git-updater' ),
+					'description' => __('Add GitHub Gist hosted repositories to the Git Updater plugin.','git-updater'),
 					'host'     => 'github',
 					'slug'     => 'git-updater-gist/git-updater-gist.php',
 					'uri'      => 'afragen/git-updater-gist',
@@ -70,6 +71,7 @@ class Add_Ons {
 			'bitbucket' => [
 				[
 					'name'     => __( 'Git Updater - Bitbucket', 'git-updater' ),
+					'description' => __('Add Bitbucket and Bitbucket Server repositories to the Git Updater plugin.','git-updater'),
 					'host'     => 'github',
 					'slug'     => 'git-updater-bitbucket/git-updater-bitbucket.php',
 					'uri'      => 'afragen/git-updater-bitbucket',
@@ -81,6 +83,7 @@ class Add_Ons {
 			'gitlab'    => [
 				[
 					'name'     => __( 'Git Updater - GitLab', 'git-updater' ),
+					'description' => __('Add GitLab hosted repositories to the Git Updater plugin.','git-updater'),
 					'host'     => 'github',
 					'slug'     => 'git-updater-gitlab/git-updater-gitlab.php',
 					'uri'      => 'afragen/git-updater-gitlab',
@@ -92,6 +95,7 @@ class Add_Ons {
 			'gitea'     => [
 				[
 					'name'     => __( 'Git Updater - Gitea', 'git-updater' ),
+					'description' => __('Add GitLab hosted repositories to the Git Updater plugin.','git-updater'),
 					'host'     => 'github',
 					'slug'     => 'git-updater-gitea/git-updater-gitea.php',
 					'uri'      => 'afragen/git-updater-gitea',
