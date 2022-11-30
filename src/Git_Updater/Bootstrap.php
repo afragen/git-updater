afafa7fc37b496cfb37ebe1c56f1abc4313752a4
Andy Fragen <andy@thefragens.com>
2022-11-29 21:06:37 -0800

    don't want that

--- src/Git_Updater/Bootstrap.php
+++ src/Git_Updater/Bootstrap.php
@@ -136,7 +136,7 @@ class Bootstrap {
 	 * @return void
 	 */
 	public function remove_cron_events() {
-		$crons = [ 'gu_get_remote_plugin', 'gu_get_remote_theme', 'gu_delete_access_tokens' ];
+		$crons = [ 'gu_get_remote_plugin', 'gu_get_remote_theme' ];
 		foreach ( $crons as $cron ) {
 			$timestamp = \wp_next_scheduled( $cron );
 			\wp_unschedule_event( $timestamp, $cron );
