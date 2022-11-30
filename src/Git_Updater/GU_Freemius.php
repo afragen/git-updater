2c284e80aab41ca1b8c380f4d0227cb117e56793
Andy Fragen <andy@thefragens.com>
2022-11-29 21:07:26 -0800

    update Freemius init

--- src/Git_Updater/GU_Freemius.php
+++ src/Git_Updater/GU_Freemius.php
@@ -50,31 +50,31 @@ class GU_Freemius {
 
 					$gu_fs = fs_dynamic_init(
 						[
-							'id'                  => '11525',
-							'slug'                => 'git-updater',
-							'premium_slug'        => 'git-updater',
-							'type'                => 'plugin',
-							'public_key'          => 'pk_aaa04d83b4c42470937266f9b4fca',
-							'is_premium'          => true,
-							'premium_suffix'      => 'Default',
-							// If your plugin is a serviceware, set this option to false.
-							'has_premium_version' => true,
-							'has_addons'          => false,
-							'has_paid_plans'      => true,
-							'is_org_compliant'    => false,
-							'trial'               => [
+							'id'               => '11525',
+							'slug'             => 'git-updater',
+							'premium_slug'     => 'git-updater',
+							'type'             => 'plugin',
+							'public_key'       => 'pk_aaa04d83b4c42470937266f9b4fca',
+							'is_premium'       => true,
+							'is_premium_only'  => true,
+							'has_addons'       => false,
+							'has_paid_plans'   => true,
+							'is_org_compliant' => false,
+							'trial'            => [
 								'days'               => 14,
 								'is_require_payment' => true,
 							],
-							'menu'                => [
+							'menu'             => [
 								'slug'    => 'git-updater',
-								'contact' => false,
 								'support' => false,
 								'network' => true,
 								'parent'  => [
 									'slug' => is_multisite() ? 'settings.php' : 'options-general.php',
 								],
 							],
+							// Set the SDK to work in a sandbox mode (for development & testing).
+							// IMPORTANT: MAKE SURE TO REMOVE SECRET KEY BEFORE DEPLOYMENT.
+							'secret_key'       => 'sk_Jj*#qrkDgrf&tMvR+oW?4ad?l^Q>U',
 						]
 					);
 				}
@@ -88,6 +88,7 @@ class GU_Freemius {
 			do_action( 'gu_fs_loaded' );
 		}
 		gu_fs()->add_filter( 'plugin_icon', [ $this, 'add_icon' ] );
+		gu_fs()->add_filter( 'is_submenu_visible', [ $this, 'is_submenu_visible' ], 10, 2 );
 	}
 
 	/**
@@ -98,4 +99,21 @@ class GU_Freemius {
 	public function add_icon() {
 		return dirname( __DIR__, 2 ) . '/assets/icon.svg';
 	}
+
+	/**
+	 * Show the contact submenu item only when the user have a valid non-expired license.
+	 *
+	 * @param $is_visible The filtered value. Whether the submenu item should be visible or not.
+	 * @param $menu_id    The ID of the submenu item.
+	 *
+	 * @return bool If true, the menu item should be visible.
+	 */
+	public function is_submenu_visible( $is_visible, $menu_id ) {
+		if ( 'contact' != $menu_id ) {
+			return $is_visible;
+		}
+
+		return gu_fs()->can_use_premium_code();
+	}
+
 }
