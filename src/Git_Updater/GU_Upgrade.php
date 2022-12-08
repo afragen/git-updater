91b6c531d2b1cf8e1814085719167c445c027f12
Andy Fragen <andy@thefragens.com>
2022-12-08 10:54:09 -0400

    change $db_version from int to version_compare

--- src/Git_Updater/GU_Upgrade.php
+++ src/Git_Updater/GU_Upgrade.php
@@ -30,34 +30,42 @@ class GU_Upgrade {
 	 *
 	 * @var int
 	 */
-	private $db_version = 8312; // TODO: change number.
+	private $db_version = '11.1.10.3'; // TODO: change number.
 
 	/**
 	 * Run update check against db_version.
 	 */
 	public function run() {
 		$options    = $this->get_class_vars( 'Base', 'options' );
-		$db_version = isset( $options['db_version'] ) ? (int) $options['db_version'] : 6000;
+		$db_version = isset( $options['db_version'] ) && ! is_integer( $options['db_version'] ) ? $options['db_version'] : '6.0.0';
 		$this->schedule_access_token_cleanup();
 
-		if ( $db_version === $this->db_version ) {
+		if ( version_compare( $db_version, $this->db_version, '=' ) ) {
 			return;
 		}
 
 		switch ( $db_version ) {
-			case $db_version < $this->db_version:
+			case version_compare( $db_version, $this->db_version, '<' ):
 				$this->delete_flush_cache();
+				$this->save_db_version( $options );
 				break;
 			default:
 				break;
 		}
 
+	}
+
+	/**
+	 * Save $db_version on update.
+	 *
+	 * @param array $options Array of Git Updater options.
+	 *
+	 * @return void
+	 */
+	private function save_db_version( $options ) {
 		$options = array_merge(
 			(array) $options,
-			[
-				'db_version'     => (int) $this->db_version,
-				'gu_auto_update' => '1',
-			]
+			[ 'db_version' => $this->db_version ]
 		);
 		update_site_option( 'git_updater', $options );
 	}
