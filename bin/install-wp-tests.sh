204c5cfbe1b3c4bb0b63689d7dac8c288e0bf7fc
Andy Fragen <andy@thefragens.com>
2023-04-03 15:18:52 -0700

    try setting auth plugin

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -112,7 +112,7 @@ install_db() {
 	fi
 
 	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA --authentication-plugin=mysql_native_password
 }
 
 install_wp
