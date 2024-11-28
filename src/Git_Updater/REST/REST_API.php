aafac580413ca478361c5a2a2be0e6c698f0f620
Andy Fragen <andy@thefragens.com>
2024-11-28 12:55:32 -0800

    if repo requires auth header then return auth header

--- src/Git_Updater/REST/REST_API.php
+++ src/Git_Updater/REST/REST_API.php
@@ -410,8 +410,11 @@ class REST_API {
 			'external'          => 'xxx',
 		];
 
-		$repo_api_data['auth_header'] = Singleton::get_instance( 'Fragen\Git_Updater\API\API', $this )->add_auth_header( [], $repo_api_data['download_link'] );
-		$repo_api_data['auth_header'] = Singleton::get_instance( 'Fragen\Git_Updater\API\API', $this )->unset_release_asset_auth( $repo_api_data['auth_header'], $repo_api_data['download_link'] );
+		if ( $repo_api_data['is_private'] || in_array( $repo_api_data['git'], [ 'gitlab', 'gitea' ] )
+		) {
+			$repo_api_data['auth_header'] = Singleton::get_instance( 'Fragen\Git_Updater\API\API', $this )->add_auth_header( [], $repo_api_data['download_link'] );
+			$repo_api_data['auth_header'] = Singleton::get_instance( 'Fragen\Git_Updater\API\API', $this )->unset_release_asset_auth( $repo_api_data['auth_header'], $repo_api_data['download_link'] );
+		}
 
 		if ( $download && $repo_data->release_asset ) {
 			if ( isset( $repo_cache['release_asset_redirect'] ) ) {
