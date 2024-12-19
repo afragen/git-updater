7606afaf035aa0e239082836f64527b9c665eb4a
Andy Fragen <andy@thefragens.com>
2024-12-18 17:05:50 -0800

    update conditional

--- src/Git_Updater/Traits/API_Common.php
+++ src/Git_Updater/Traits/API_Common.php
@@ -342,7 +342,7 @@ trait API_Common {
 			}
 		}
 
-		if ( $response && ! $this->response['release_asset'] ) {
+		if ( $response && ! isset( $this->response['release_asset'] ) ) {
 			$this->set_repo_cache( 'release_asset', $response );
 		}
 
