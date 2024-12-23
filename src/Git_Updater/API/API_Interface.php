2be1ed9ad5feb253d5880daa195762b04caed0a7
Andy Fragen <andy@thefragens.com>
2024-12-23 12:05:07 -0800

    update docblock

--- src/Git_Updater/API/API_Interface.php
+++ src/Git_Updater/API/API_Interface.php
@@ -39,7 +39,7 @@ interface API_Interface {
 	 *
 	 * @access public
 	 *
-	 * @param string $changes Changelog filename - (deprecated).
+	 * @param null $changes Changelog filename - (deprecated).
 	 *
 	 * @return mixed
 	 */
