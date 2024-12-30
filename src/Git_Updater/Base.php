885b36d38acd68bae515c19c65fc1e281be68fcd
Andy Fragen <andy@thefragens.com>
2024-12-30 13:18:23 -0800

    get contents everywhere

--- src/Git_Updater/Base.php
+++ src/Git_Updater/Base.php
@@ -346,9 +346,7 @@ class Base {
 
 		if ( $repo_api->get_remote_info( $file ) ) {
 			if ( ! self::is_wp_cli() ) {
-				if ( in_array( $repo->git, [ 'github','gitea' ], true ) ) {
-					$repo_api->get_repo_contents();
-				}
+				$repo_api->get_repo_contents();
 				$repo_api->get_remote_readme();
 				$repo_api->get_remote_changes( '' );
 				$repo_api->get_repo_meta();
