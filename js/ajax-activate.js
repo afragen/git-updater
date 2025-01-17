999ade11206b18c0369898e16050906a36ef8fb0
Andy Fragen <andy@thefragens.com>
2025-01-17 15:26:29 -0800

    Colin adds queue for Activate button display

--- js/ajax-activate.js
+++ js/ajax-activate.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+(function( $, wp ) {
 	var $pluginFilter = $( "#plugin-filter, #plugin-information-footer" ),
 		__            = wp.i18n.__;
 		_x            = wp.i18n._x,
@@ -33,4 +33,52 @@
 			}
 		);
 	});
+
+	/**
+	 * Pulls available jobs from the queue and runs them.
+	 *
+	 * @since 4.2.0
+	 * @since 4.6.0 Can handle multiple job types.
+	 */
+	wp.updates.queueChecker = function() {
+		var job;
+
+		if ( wp.updates.ajaxLocked || ! wp.updates.queue.length ) {
+			return;
+		}
+
+		job = wp.updates.queue.shift();
+
+		// Handle a queue job.
+		switch ( job.action ) {
+			case 'install-plugin':
+				wp.updates.installPlugin( job.data );
+				break;
+
+			case 'update-plugin':
+				wp.updates.updatePlugin( job.data );
+				break;
+
+			case 'delete-plugin':
+				wp.updates.deletePlugin( job.data );
+				break;
+
+			case 'install-theme':
+				wp.updates.installTheme( job.data );
+				break;
+
+			case 'update-theme':
+				wp.updates.updateTheme( job.data );
+				break;
+
+			case 'delete-theme':
+				wp.updates.deleteTheme( job.data );
+				break;
+			case 'check_plugin_dependencies':
+				wp.updates.checkPluginDependencies( job.data );
+				break;
+			default:
+				break;
+		}
+	};
 })( jQuery, window.wp, window._wpUpdatesSettings );
