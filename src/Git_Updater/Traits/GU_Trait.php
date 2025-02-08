b62bbcfaf6b73e3a05975b02cc328d17fc46a579
Andy Fragen <andy@thefragens.com>
2025-02-07 20:40:40 -0800

    just a little bit safer

--- src/Git_Updater/Traits/GU_Trait.php
+++ src/Git_Updater/Traits/GU_Trait.php
@@ -484,7 +484,9 @@ trait GU_Trait {
 		$rename = implode( '-', $rename );
 
 		// For AJAX install, not from Install tab, need to exit as slug is correct. Refer to Add-Ons.
-		if ( ! isset( $_POST['git_updater_repo'] ) && isset( $_POST['action'] ) && check_ajax_referer( 'updates' ) ) {
+		if ( ( ! isset( $_POST['git_updater_repo'] ) && isset( $_POST['action'] ) )
+			&& ( wp_doing_ajax() && check_ajax_referer( 'updates' ) )
+		) {
 			if ( str_contains( sanitize_key( wp_unslash( $_POST['action'] ) ), 'install' ) ) {
 				$arr['slug'] = $slug;
 			}
