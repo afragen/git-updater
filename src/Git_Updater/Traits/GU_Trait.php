e959ea9f87320abf75a7248d7319482b4df62cae
Andy Fragen <andy@thefragens.com>
2025-02-07 17:13:05 -0800

    update checks for AJAX install

--- src/Git_Updater/Traits/GU_Trait.php
+++ src/Git_Updater/Traits/GU_Trait.php
@@ -483,9 +483,9 @@ trait GU_Trait {
 		array_pop( $rename );
 		$rename = implode( '-', $rename );
 
-		// For AJAX install need to exit as slug is correct. Refer to Add-Ons.
-		if ( check_ajax_referer( 'updates' ) ) {
-			if ( isset( $_POST['action'] ) && str_contains( sanitize_key( wp_unslash( $_POST['action'] ) ), 'install' ) ) {
+		// For AJAX install, not from Install tab, need to exit as slug is correct. Refer to Add-Ons.
+		if ( ! isset( $_POST['git_updater_repo'] ) && isset( $_POST['action'] ) && check_ajax_referer( 'updates' ) ) {
+			if ( str_contains( sanitize_key( wp_unslash( $_POST['action'] ) ), 'install' ) ) {
 				return $arr;
 			}
 		}
