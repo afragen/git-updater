a8e188cfff15f0ce7b87215a8a69840f09d70365
Andy Fragen <andy@thefragens.com>
2025-07-10 09:23:18 -0700

    use get_file_without_did_hash() here

--- src/Git_Updater/Bootstrap.php
+++ src/Git_Updater/Bootstrap.php
@@ -121,14 +121,16 @@ class Bootstrap {
 		}
 
 		// Strip hash from slug and re-make file.
-		$hook       = current_action();
-		$file       = str_replace( 'activate_', '', $hook );
-		$file       = explode( '/', $file, 2 );
-		$slug_parts = explode( '-', $file[0] );
-		array_pop( $slug_parts );
-		$slug = implode( '-', $slug_parts );
+		$hook = current_action();
+		$file = str_replace( 'activate_', '', $hook );
+		// $file       = explode( '/', $file, 2 );
+		// $slug_parts = explode( '-', $file[0] );
+		// array_pop( $slug_parts );
+		// $slug = implode( '-', $slug_parts );
 
-		if ( $slug && 'git-updater/git-updater.php' !== $slug . '/' . $file[1] ) {
+		$file = $this->get_file_without_did_hash( 'did:plc:afjf7gsjzsqmgc7dlhb553mv', $file );
+
+		if ( $slug && 'git-updater/git-updater.php' !== $file ) {
 			require_once __DIR__ . '/Shim.php';
 			$result = move_dir( $plugin_dir . dirname( $slug ), $plugin_dir . 'git-updater', true );
 			if ( is_wp_error( $result ) ) {
