1bb1faa686d2931e744f09a8324af6fd4e89ae42
Andy Fragen <andy@thefragens.com>
2025-08-02 21:55:16 -0700

    ignore keys with leading 'v' and skip if contain dash or other letters

--- src/Git_Updater/Traits/API_Common.php
+++ src/Git_Updater/Traits/API_Common.php
@@ -72,8 +72,12 @@ trait API_Common {
 			} else {
 				$release_assets = [];
 				foreach ( $response as $release ) {
+					// Ignore leading 'v' and skip anything with dash or words.
+					if ( ! preg_match( '/[^v]+[-a-z]+/', $release->tag_name ) ) {
 					$release_assets[ $release->tag_name ] = $release->assets[0]->url ?? '';
 				}
+				}
+
 
 				return $release_assets;
 			}
