58f9fc0269aef30ba1973a09cf62d23fef4e460f
Andy Fragen <andy@thefragens.com>
2026-01-07 12:00:41 -0800

    more data

--- src/Git_Updater/REST/REST_API.php
+++ src/Git_Updater/REST/REST_API.php
@@ -506,6 +506,7 @@ class REST_API {
 			'update_uri'        => $repo_data->update_uri ?? '',
 			'is_private'        => $repo_data->is_private,
 			'dot_org'           => $repo_data->dot_org,
+			'dev_channel'       => $channel,
 			'use_dev_channel'   => $channel && $use_channel,
 			'release_asset'     => $repo_data->release_asset,
 			'version'           => $repo_data->remote_version,
