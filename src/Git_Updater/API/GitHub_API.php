b9fd6f0aa0d856d0015113c46e080cd6d66b7bda
Andy Fragen <andy@thefragens.com>
2026-07-24 08:44:31 -0700

    phpstan error, not necessary

--- src/Git_Updater/API/GitHub_API.php
+++ src/Git_Updater/API/GitHub_API.php
@@ -397,12 +397,6 @@ class GitHub_API extends API implements API_Interface {
 	protected function parse_contents_response( $response ) {
 		$files = [];
 		$dirs  = [];
-		if ( is_scalar( $response ) ) {
-			return [
-				'files' => $files,
-				'dirs'  => $dirs,
-			];
-		}
 
 		foreach ( $response as $content ) {
 			$content = (object) $content;
